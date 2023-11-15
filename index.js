e2b5c192a88fb00f209c8b51e8b49e4accbc06f6
Garvit Agrawal <garvit.agrawal.che20@itbhu.ac.in>
2023-11-16 01:45:07 +0530

    first  commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// script.js
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "jj4sh7y23i");
+
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-8P8CR90ZHF");
+
+// Other scripts or imports
